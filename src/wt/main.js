d596848c243ca949f990a0059149d3ffc51b8c83
OlgaKrukova <krukovaolga1@gmail.com>
2024-10-07 17:24:06 +0300

    feat: implement worker threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCores = os.cpus().length;
+  const workers = [];
+
+  for (let i = 0; i < numCores; i++) {
+    workers.push(
+      new Promise((resolve) => {
+        const worker = new Worker(path.join(__dirname, 'worker.js'), {
+          workerData: 10 + i,
+        });
+        worker.on('message', (result) => {
+          resolve({ status: 'resolved', data: result });
+        });
+        worker.on('error', () => {
+          resolve({ status: 'error', data: null });
+        });
+      }),
+    );
+  }
+
+  const workerResults = await Promise.all(workers);
+  console.log(workerResults);
 };
 
 await performCalculations();
